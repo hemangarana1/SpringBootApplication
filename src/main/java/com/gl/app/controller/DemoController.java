8cd47c0499a1a8a2535857938c30a2a42caa096e
hemangarana1 <hemanga.rana@globallogic.com>
2024-06-07 07:41:19 +0530

    First Commit

---
+++ src/main/java/com/gl/app/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.gl.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        System.out.println("Hello Spring Boot Boot Boot Application");
+        System.out.println("Hello Spring Boot");
+        return "Hello from Spring Boot!";
+    }
+}
